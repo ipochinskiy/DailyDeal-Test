581b9ae71dc5e4162399b9117a4e0b64e41f5c2c
Pochinskii Ilia <zorkiypnz@yandex.ru>
2016-08-06 05:15:08 +0200

    moved mouthClassNameMap into constants

--- client/components/Emoji.jsx
+++ client/components/Emoji.jsx
@@ -3,33 +3,7 @@
 import { Meteor } from 'meteor/meteor';
 import React, {Component} from 'react';
 
-const mouthClassNameMap = {
-  'awful': colored => {
-    return !!colored
-      ? [ 'awful', 'mouth-opened-colored' ]
-      : [ 'awful', 'mouth-opened-inactive' ];
-  },
-  'good': colored => {
-    return !!colored
-      ? [ 'good', 'good-colored' ]
-      : [ 'good', 'good-inactive' ];
-  },
-  'neutral': colored => {
-    return !!colored
-      ? [ 'neutral', 'neutral-colored' ]
-      : [ 'neutral', 'neutral-inactive' ];
-  },
-  'bad': colored => {
-    return !!colored
-      ? [ 'bad', 'bad-colored' ]
-      : [ 'bad', 'bad-inactive' ];
-  },
-  'amazing': colored => {
-    return !!colored
-      ? [ 'amazing', 'mouth-opened-colored' ]
-      : [ 'amazing', 'mouth-opened-inactive' ];
-  }
-}
+import { mouthClassNameMap } from '../../libs/constants';
 
 const getMouthClassNames = (mood, colored) => {
   let fn = mouthClassNameMap[mood];
@@ -58,8 +32,8 @@ export default class Emoji extends Component {
       }
     }
     render() {
-      var emojisClassNames = getEmojiClassNames(this.props.colored).join(' ');
-      var mouthClassNames = getMouthClassNames(this.props.mood, this.props.colored).join(' ');
+      let emojisClassNames = getEmojiClassNames(this.props.colored).join(' ');
+      let mouthClassNames = getMouthClassNames(this.props.mood, this.props.colored).join(' ');
       return (
         <div className={ emojisClassNames } onClick={ () => this.rate(this.props.mark) }>
           <div className={ mouthClassNames } ></div>
--- libs/constants.js
+++ libs/constants.js
@@ -5,3 +5,31 @@ export const emojiMarkMap = {
 	'good': 4,
 	'amazing': 5,
 }
+
+export const mouthClassNameMap = {
+  'awful': colored => {
+    return !!colored
+      ? [ 'awful', 'mouth-opened-colored' ]
+      : [ 'awful', 'mouth-opened-inactive' ];
+  },
+  'good': colored => {
+    return !!colored
+      ? [ 'good', 'good-colored' ]
+      : [ 'good', 'good-inactive' ];
+  },
+  'neutral': colored => {
+    return !!colored
+      ? [ 'neutral', 'neutral-colored' ]
+      : [ 'neutral', 'neutral-inactive' ];
+  },
+  'bad': colored => {
+    return !!colored
+      ? [ 'bad', 'bad-colored' ]
+      : [ 'bad', 'bad-inactive' ];
+  },
+  'amazing': colored => {
+    return !!colored
+      ? [ 'amazing', 'mouth-opened-colored' ]
+      : [ 'amazing', 'mouth-opened-inactive' ];
+  }
+}
