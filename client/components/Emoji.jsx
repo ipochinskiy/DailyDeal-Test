f18197b1df6b65b461e2b7749687d678c402d5b1
Pochinskii Ilia <zorkiypnz@yandex.ru>
2016-08-06 05:08:22 +0200

    added styles for colored emojis

--- client/components/Emoji.jsx
+++ client/components/Emoji.jsx
@@ -3,6 +3,45 @@
 import { Meteor } from 'meteor/meteor';
 import React, {Component} from 'react';
 
+const mouthClassNameMap = {
+  'awful': colored => {
+    return !!colored
+      ? [ 'awful', 'mouth-opened-colored' ]
+      : [ 'awful', 'mouth-opened-inactive' ];
+  },
+  'good': colored => {
+    return !!colored
+      ? [ 'good', 'good-colored' ]
+      : [ 'good', 'good-inactive' ];
+  },
+  'neutral': colored => {
+    return !!colored
+      ? [ 'neutral', 'neutral-colored' ]
+      : [ 'neutral', 'neutral-inactive' ];
+  },
+  'bad': colored => {
+    return !!colored
+      ? [ 'bad', 'bad-colored' ]
+      : [ 'bad', 'bad-inactive' ];
+  },
+  'amazing': colored => {
+    return !!colored
+      ? [ 'amazing', 'mouth-opened-colored' ]
+      : [ 'amazing', 'mouth-opened-inactive' ];
+  }
+}
+
+const getMouthClassNames = (mood, colored) => {
+  let fn = mouthClassNameMap[mood];
+  return fn ? fn(colored) : [];
+}
+
+const getEmojiClassNames = colored => {
+  return !!colored
+    ? [ 'emoji', 'emoji-colored' ]
+    : [ 'emoji', 'emoji-inactive' ];
+}
+
 export default class Emoji extends Component {
     rate(mark) {
       Meteor.call('rate', mark);
@@ -19,11 +58,13 @@ export default class Emoji extends Component {
       }
     }
     render() {
-        return (
-          <div className="emoji" onClick={ () => this.rate(this.props.mark) }>
-            <div className={ this.props.type } ></div>
-            { this.renderCheck() }
-          </div>
-        );
+      var emojisClassNames = getEmojiClassNames(this.props.colored).join(' ');
+      var mouthClassNames = getMouthClassNames(this.props.mood, this.props.colored).join(' ');
+      return (
+        <div className={ emojisClassNames } onClick={ () => this.rate(this.props.mark) }>
+          <div className={ mouthClassNames } ></div>
+          { this.renderCheck() }
+        </div>
+      );
     }
 }
