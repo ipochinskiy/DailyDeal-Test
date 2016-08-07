a042e31a18eb3fd1b2f00a253031d22ca37cc0c2
Pochinskii Ilia <zorkiypnz@yandex.ru>
2016-08-07 08:51:39 +0200

    applied fields filtering in publication

--- server/publications/Ratings.js
+++ server/publications/Ratings.js
@@ -2,4 +2,9 @@ import { Meteor } from 'meteor/meteor';
 
 import { Ratings } from '../../libs/collections/Ratings';
 
-Meteor.publish('rating', () => Ratings.find({}, { sort: { 'ratedAt': 1 } }));
+Meteor.publish('rating', function() {
+  return Ratings.find({}, {
+    sort: { 'ratedAt': 1 },
+    fields: { remoteAddress: 0 }
+  });
+});
