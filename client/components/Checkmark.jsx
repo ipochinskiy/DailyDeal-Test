419a3d0ef2a30ddc3b84b3847591276aa0e5853f
Pochinskii Ilia <zorkiypnz@yandex.ru>
2016-08-07 13:12:21 +0200

    improved animation for checkmark

--- client/components/Checkmark.jsx
+++ client/components/Checkmark.jsx
@@ -6,8 +6,7 @@ import React, {Component} from 'react';
 export default class Checkmark extends Component {
     render() {
       return (
-        <div className="checkmark rotate appear-animation">
-          <div className="checkmark_circle"></div>
+        <div className="checkmark_circle rotate fadein-animation">
           <div className="checkmark_stem"></div>
           <div className="checkmark_kick"></div>
         </div>
